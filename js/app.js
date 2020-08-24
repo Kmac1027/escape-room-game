6c125c5a4e8b635c7aafb20947c434249caca09d
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-24 13:54:41 -0700

    added requirmnets

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict'
+
+var life = '';
+
+function checkLocalStorageForLife(){
+  if (localStorage.getItems('life') === null){
+    life = 10
+  } else {
+    var getLifeValue = localStorage.getItem('life');
+    var parseLife = JSON.parse(getLifeValue);
+    life = parseLife; 
+  }
+}
+// var jsonLife = JSON.stringify(life);
+// localStorage.setItems('life', jsonLife);
