f7296735086f2c574405ef459057ff657ff09934
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-24 18:41:56 -0700

    home page link added

--- js/frontpage.js
+++ js/frontpage.js
@@ -6,4 +6,10 @@ form.addEventListener('submit', function (event) {
   var playerName = event.target.playername.value;
   var jsonPlayerName = JSON.stringify(playerName);
   localStorage.setItem('playerName', jsonPlayerName);
+  var a = document.createElement('a')
+  var link = document.createTextNode('Click Here to Enter');
+  a.appendChild(link);
+  a.title = "link to first room";
+  a.href = "../pages/firstroom.html";
+  form.appendChild(a);
 })
